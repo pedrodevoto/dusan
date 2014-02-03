c4a448c4d43edbfa138520180a9d23d719702631
Pedro Devoto <pedro.devoto@gmail.com>
2014-02-03 11:16:33 -0300

    mas fix clientes

--- insert-cliente.php
+++ insert-cliente.php
@@ -12,7 +12,7 @@
 	if ((isset($_POST["box-insert"])) && ($_POST["box-insert"] == "1")) {	
 		
 		// Insert
-		$insertSQL = sprintf("INSERT INTO cliente (cliente_tipo_persona, cliente_nombre, cliente_apellido, cliente_razon_social, cliente_tipo_sociedad_id, cliente_nacimiento, cliente_sexo, cliente_tipo_doc, cliente_nro_doc, cliente_nacionalidad_id, cliente_cf_id, cliente_registro, cliente_reg_vencimiento, cliente_cuit_0, cliente_cuit_1, cliente_cuit_2, cliente_email, cliente_email_alt) VALUES (%s, UPPER(TRIM(%s)), UPPER(TRIM(%s)), UPPER(TRIM(%s)), %s, %s, %s, %s, TRIM(%s), %s, %s, TRIM(%s), %s, TRIM(%s), TRIM(%s), TRIM(%s))",
+		$insertSQL = sprintf("INSERT INTO cliente (cliente_tipo_persona, cliente_nombre, cliente_apellido, cliente_razon_social, cliente_tipo_sociedad_id, cliente_nacimiento, cliente_sexo, cliente_tipo_doc, cliente_nro_doc, cliente_nacionalidad_id, cliente_cf_id, cliente_registro, cliente_reg_vencimiento, cliente_cuit_0, cliente_cuit_1, cliente_cuit_2, cliente_email, cliente_email_alt) VALUES (%s, UPPER(TRIM(%s)), UPPER(TRIM(%s)), UPPER(TRIM(%s)), %s, %s, %s, %s, TRIM(%s), %s, %s, TRIM(%s), %s, TRIM(%s), TRIM(%s), TRIM(%s), TRIM(%s), TRIM(%s))",
 						GetSQLValueString($_POST['box-cliente_tipo_persona'], "int"),
 						GetSQLValueString($_POST['box-cliente_nombre'], "text"),
 						GetSQLValueString($_POST['box-cliente_apellido'], "text"),
@@ -39,7 +39,7 @@
 					$sql = 'INSERT INTO cliente_cliente_reg_tipo (cliente_id, cliente_reg_tipo_id) VALUES ('.$cliente_id.', '.intval($cliente_reg_tipo_id).')';
 					mysql_query($sql, $connection) or die(mysql_error());
 				}
-				echo 'El registro ha sido insertado con éxito | <a href="javascript:openBoxContacto('.mysql_insert_id().')" class="lnkBox">ALTA DE CONTACTOS</a>';
+				echo 'El registro ha sido insertado con éxito | <a href="javascript:openBoxContacto('.$cliente_id.')" class="lnkBox">ALTA DE CONTACTOS</a>';
 				break;
 			case 1062:
 				echo 'Error: Registro duplicado.';
