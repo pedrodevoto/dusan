d5255fd628b2dc351347d3650e6aea1bc63ce614
Pedro Devoto <pedro.devoto@gmail.com>
2014-03-14 16:17:20 -0300

    fiz home

--- main.php
+++ main.php
@@ -25,7 +25,7 @@
 		<?php include('inc/header.php'); ?>    
 		<div id="divMain">
 			<p class="txtMain">Bienvenido al panel de administración, <strong><?php echo($_SESSION['ADM_Username']); ?></strong>.</p>          
-			<div style="margin-top:100px">
+			<div style="margin-top:80px">
 				<a href='#' id="btncrearcliente"><img width="180px" height="180px" src="media/images/crearcliente.jpg" /></a>
 				<a href='#' id="btncrearautomotor"><img width="180px" height="180px" src="media/images/crearautomotor.jpg" /></a>
 				<a href='#' id="btncrearpolizapersonas"><img width="180px" height="180px" src="media/images/crearpolizapersonas.jpg" /></a>
