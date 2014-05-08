e10eeda9ddbe1baf289d75f8ceeb7c4a84bacb8e
Pedro Devoto <pedro.devoto@gmail.com>
2014-05-08 20:03:26 -0300

    fix en creacion de codigo de productor

--- insert-prodseg.php
+++ insert-prodseg.php
@@ -12,10 +12,9 @@
 	if (isset($_POST["box-productor_id"]) && $_POST["box-productor_id"]!="") {
 		
 		// Insert
-		$insertSQL = sprintf("INSERT INTO productor_seguro (productor_id, seguro_id, sucursal_id, productor_seguro_codigo, productor_seguro_organizacion_flag, productor_seguro_organizacion_nombre, productor_seguro_organizacion_tipo_persona, productor_seguro_organizacion_matricula, productor_seguro_organizacion_cuit) VALUES (%s, %s, %s, UPPER(TRIM(%s)), %s, UPPER(TRIM(%s)), %s, UPPER(TRIM(%s)), UPPER(TRIM(%s)))",
+		$insertSQL = sprintf("INSERT INTO productor_seguro (productor_id, seguro_id, productor_seguro_codigo, productor_seguro_organizacion_flag, productor_seguro_organizacion_nombre, productor_seguro_organizacion_tipo_persona, productor_seguro_organizacion_matricula, productor_seguro_organizacion_cuit) VALUES (%s, %s, UPPER(TRIM(%s)), %s, UPPER(TRIM(%s)), %s, UPPER(TRIM(%s)), UPPER(TRIM(%s)))",
 						GetSQLValueString($_POST['box-productor_id'], "int"),
 						GetSQLValueString($_POST['box-seguro_id'], "int"),						
-						GetSQLValueString($_POST['box-sucursal_id'], "int"),						
 						GetSQLValueString($_POST['box-productor_seguro_codigo'], "text"),
 						GetSQLValueString(isset($_POST['box-productor_seguro_organizacion_flag']) ? 'true' : '', 'defined','1','0'),
 						GetSQLValueString($_POST['box-productor_seguro_organizacion_nombre'], "text"),
