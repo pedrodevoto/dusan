7e501196c03f5f40824f93a6522e9c846f9c54e2
Pedro Devoto <pedro.devoto@gmail.com>
2014-08-19 09:57:40 -0300

    bug en print-poliza

--- print-poliza.php
+++ print-poliza.php
@@ -27,7 +27,7 @@
 	$poliza_id = intval($_GET['id']);
 	
 	// Recordset: Main
-	$query_Recordset1 = sprintf("SELECT *, IF(cliente_tipo_persona=1, TRIM(CONCAT(IFNULL(cliente_apellido, ''), ' ', IFNULL(cliente_nombre, ''))), cliente_razon_social) as cliente_nombre FROM poliza JOIN (subtipo_poliza, tipo_poliza, cliente, productor_seguro, productor, seguro) ON (poliza.subtipo_poliza_id=subtipo_poliza.subtipo_poliza_id AND subtipo_poliza.tipo_poliza_id=tipo_poliza.tipo_poliza_id AND poliza.cliente_id=cliente.cliente_id AND poliza.productor_seguro_id=productor_seguro.productor_seguro_id AND productor_seguro.productor_id=productor.productor_id AND productor_seguro.seguro_id=seguro.seguro_id) LEFT JOIN (contacto, localidad) ON (poliza.cliente_id=contacto.cliente_id AND contacto_default=1 AND localidad.localidad_id = contacto.localidad_id) JOIN sucursal ON poliza.sucursal_id = sucursal.sucursal_id LEFT JOIN (poliza_plan, poliza_pack) ON poliza.poliza_plan_id = poliza_plan.poliza_plan_id AND poliza.poliza_pack_id = poliza_pack.poliza_pack_id
+	$query_Recordset1 = sprintf("SELECT *, IF(cliente_tipo_persona=1, TRIM(CONCAT(IFNULL(cliente_apellido, ''), ' ', IFNULL(cliente_nombre, ''))), cliente_razon_social) as cliente_nombre FROM poliza JOIN (subtipo_poliza, tipo_poliza, cliente, productor_seguro, productor, seguro) ON (poliza.subtipo_poliza_id=subtipo_poliza.subtipo_poliza_id AND subtipo_poliza.tipo_poliza_id=tipo_poliza.tipo_poliza_id AND poliza.cliente_id=cliente.cliente_id AND poliza.productor_seguro_id=productor_seguro.productor_seguro_id AND productor_seguro.productor_id=productor.productor_id AND productor_seguro.seguro_id=seguro.seguro_id) LEFT JOIN contacto ON poliza.cliente_id=contacto.cliente_id AND contacto_default=1 LEFT JOIN localidad ON localidad.localidad_id = contacto.localidad_id JOIN sucursal ON poliza.sucursal_id = sucursal.sucursal_id LEFT JOIN (poliza_plan, poliza_pack) ON poliza.poliza_plan_id = poliza_plan.poliza_plan_id AND poliza.poliza_pack_id = poliza_pack.poliza_pack_id
 									WHERE poliza.poliza_id=%s GROUP BY poliza.poliza_id",
 									$poliza_id);
 	$Recordset1 = mysql_query($query_Recordset1, $connection) or die(mysql_die());
@@ -39,7 +39,7 @@
 		die("Error: Poliza no encontrada.");
 	}	
 	// If no contact info found
-	if (is_null($row_Recordset1['contacto_domicilio']) || is_null($row_Recordset1['contacto_nro'])) {
+	if (is_null($row_Recordset1['contacto_id'])) {
 		die("Error: El cliente no tiene un contacto primario asignado.");
 	}
 	
