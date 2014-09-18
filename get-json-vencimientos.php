e762eb65a5ac6728d00327d2991355a94a3d34a5
Pedro Devoto <pedro.devoto@gmail.com>
2014-09-18 14:10:36 -0300

    fix

--- get-json-vencimientos.php
+++ get-json-vencimientos.php
@@ -10,7 +10,7 @@
 <?php
 	// Recordset: Main	
 	
-	$sql = sprintf('SELECT cuota_vencimiento, poliza_numero, cuota_nro, poliza_cant_cuotas FROM cuota JOIN poliza USING (poliza_id) left join (endoso, endoso_tipo) ON (poliza.poliza_id = endoso.poliza_id AND endoso.endoso_tipo_id = endoso_tipo.endoso_tipo_id AND endoso_tipo_grupo_id = 1) WHERE endoso_id is null and cuota_vencimiento BETWEEN %s AND DATE(%s)-interval 1 day AND cuota_estado_id = 1', GetSQLValueString($_GET['start'], 'date'), GetSQLValueString($_GET['end'], 'date'));
+	$sql = sprintf('SELECT cuota_vencimiento, COALESCE(poliza_numero, "XX"), cuota_nro, poliza_cant_cuotas FROM cuota JOIN poliza USING (poliza_id) left join (endoso, endoso_tipo) ON (poliza.poliza_id = endoso.poliza_id AND endoso.endoso_tipo_id = endoso_tipo.endoso_tipo_id AND endoso_tipo_grupo_id = 1) WHERE endoso_id is null and cuota_vencimiento BETWEEN %s AND DATE(%s)-interval 1 day AND cuota_estado_id = 1', GetSQLValueString($_GET['start'], 'date'), GetSQLValueString($_GET['end'], 'date'));
 	$res = mysql_query($sql, $connection) or die(mysql_die());
 	error_log($sql);
 	$output = array();
