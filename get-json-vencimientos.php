18fa70e6d24d1e60057bd6ba6b95b86fb5be2250
Pedro Devoto <pedro.devoto@gmail.com>
2014-09-19 16:08:46 -0300

    prueba de vencimientos

--- get-json-vencimientos.php
+++ get-json-vencimientos.php
@@ -9,16 +9,23 @@
 ?>
 <?php
 	// Recordset: Main	
-	
-	$sql = sprintf('SELECT cuota_vencimiento, COUNT(cuota_id) from cuota join poliza using (poliza_id) left join (endoso, endoso_tipo) ON (poliza.poliza_id = endoso.poliza_id AND endoso.endoso_tipo_id = endoso_tipo.endoso_tipo_id AND endoso_tipo_grupo_id = 1) WHERE endoso_id is null and cuota_vencimiento BETWEEN %s AND DATE(%s)-interval 1 day AND cuota_estado_id = 1 GROUP BY cuota_vencimiento', GetSQLValueString($_GET['start'], 'date'), GetSQLValueString($_GET['end'], 'date'));
-	$res = mysql_query($sql, $connection) or die(mysql_die());
-	error_log($sql);
 	$output = array();
-	while ($row = mysql_fetch_array($res)) {
-		$output[] = array('title'=>sprintf('Vencen %s cuotas', $row[1]), 'start'=>$row[0]);
+	if (!empty($_GET['date'])) {
+		$sql = sprintf('SELECT IFNULL(poliza_numero, "-") poliza_numero, cuota_nro, poliza_cant_cuotas, cuota_monto from cuota join poliza USING (poliza_id) left join (endoso, endoso_tipo) ON (poliza.poliza_id = endoso.poliza_id AND endoso.endoso_tipo_id = endoso_tipo.endoso_tipo_id AND endoso_tipo_grupo_id = 1) where endoso_id is null and cuota_vencimiento = %s and cuota_estado_id = 1', GetSQLValueString($_GET['date'], 'date'));
+		$res = mysql_query($sql, $connection) or die(mysql_error());
+		while ($row = mysql_fetch_assoc($res)) {
+			$output[] = $row;
+		}
+	}
+	else {
+		$sql = sprintf('SELECT cuota_vencimiento, COUNT(cuota_id) from cuota join poliza using (poliza_id) left join (endoso, endoso_tipo) ON (poliza.poliza_id = endoso.poliza_id AND endoso.endoso_tipo_id = endoso_tipo.endoso_tipo_id AND endoso_tipo_grupo_id = 1) WHERE endoso_id is null and cuota_vencimiento BETWEEN %s AND DATE(%s)-interval 1 day AND cuota_estado_id = 1 GROUP BY cuota_vencimiento', GetSQLValueString($_GET['start'], 'date'), GetSQLValueString($_GET['end'], 'date'));
+		$res = mysql_query($sql, $connection) or die(mysql_die());
+		while ($row = mysql_fetch_array($res)) {
+			$output[] = array('title'=>sprintf('Vencimientos (%s)', $row[1]), 'start'=>$row[0], 'id'=>$row[0]);
+		}
 	}
 	echo json_encode($output);
-
+	
 	// Close Recordset: Main	
 	mysql_free_result($res);
 ?>
--- section-calendario.php
+++ section-calendario.php
@@ -13,6 +13,10 @@
 	
 	<script>
 	$(document).ready(function() {
+	    $( "#eventdialog" ).dialog({
+	      autoOpen: false,
+	    });
+		
 		$('#calendar').fullCalendar({
 			header: {
 					left: 'prev,next today',
@@ -24,9 +28,37 @@
 			eventLimit: true,
 			eventSources: [
 				{
-					url: 'get-json-vencimientos.php'
+					url: 'get-json-vencimientos.php',
+					className: 'vencimientos'
 				}
-			]
+			],
+			eventClick: function(event, jsEvent, view) {
+				$('#eventdialog').html('Cargando...');
+				if ($(this).hasClass('vencimientos')) {
+					$.ajax({
+						url: "get-json-vencimientos.php?date="+event.id,
+						dataType: 'json',
+						success: function (j) {
+							var output = '';
+							output += '<table class="tblBox">';
+							$.each(j, function (key, object) {
+								output += '<tr>';
+								output += '<td>PZA '+object.poliza_numero+'</td>';
+								output += '<td>Cuota '+object.cuota_nro+'/'+object.poliza_cant_cuotas+'</td>';
+								output += '<td>$'+object.cuota_monto+'</td>';
+								output += '</tr>';
+							});
+							output += '</table>';
+							$('#eventdialog').html(output);
+						}
+					});
+					date = moment(event.id, 'YYYY-MM-DD');
+					$("#eventdialog").dialog({
+						position: { my: "left top", at: "left top", of: $(jsEvent.srcElement)},
+						title: 'Venimientos el '+date.format('DD/MM/YY')
+					}).dialog("open");
+				}
+			}
 		})
 	});
 	</script>                        
@@ -35,7 +67,9 @@
 			max-width: 900px;
 			margin: 0 auto;
 		}
-	
+	.fc-event {
+		cursor:pointer;
+	}
 	</style>
 </head>
 <body>
@@ -44,6 +78,9 @@
 		<?php include('inc/header.php'); ?>    
 		<div id="divMain">
 			<div id='calendar' style="margin-top:30px"></div>
+			<div id="eventdialog">
+			  
+			</div>
 		</div>
 	</div>
 </body>
