fd9b9df3bbea453eda7f9edbc4881f6933f0cf3e
Pedro Devoto <pedro.devoto@gmail.com>
2014-09-19 11:31:30 -0300

    agrupar los vencimientos del calendar

--- get-json-vencimientos.php
+++ get-json-vencimientos.php
@@ -10,12 +10,12 @@
 <?php
 	// Recordset: Main	
 	
-	$sql = sprintf('SELECT cuota_vencimiento, COALESCE(poliza_numero, "XX"), cuota_nro, poliza_cant_cuotas FROM cuota JOIN poliza USING (poliza_id) left join (endoso, endoso_tipo) ON (poliza.poliza_id = endoso.poliza_id AND endoso.endoso_tipo_id = endoso_tipo.endoso_tipo_id AND endoso_tipo_grupo_id = 1) WHERE endoso_id is null and cuota_vencimiento BETWEEN %s AND DATE(%s)-interval 1 day AND cuota_estado_id = 1', GetSQLValueString($_GET['start'], 'date'), GetSQLValueString($_GET['end'], 'date'));
+	$sql = sprintf('SELECT cuota_vencimiento, COUNT(cuota_id) from cuota join poliza using (poliza_id) left join (endoso, endoso_tipo) ON (poliza.poliza_id = endoso.poliza_id AND endoso.endoso_tipo_id = endoso_tipo.endoso_tipo_id AND endoso_tipo_grupo_id = 1) WHERE endoso_id is null and cuota_vencimiento BETWEEN %s AND DATE(%s)-interval 1 day AND cuota_estado_id = 1 GROUP BY cuota_vencimiento', GetSQLValueString($_GET['start'], 'date'), GetSQLValueString($_GET['end'], 'date'));
 	$res = mysql_query($sql, $connection) or die(mysql_die());
 	error_log($sql);
 	$output = array();
 	while ($row = mysql_fetch_array($res)) {
-		$output[] = array('title'=>sprintf('Vencimiento PZA %s %s/%s', $row[1], $row[2], $row[3]), 'start'=>$row[0]);
+		$output[] = array('title'=>sprintf('Vencen %s cuotas', $row[1]), 'start'=>$row[0]);
 	}
 	echo json_encode($output);
 
