ab94d595912d8f7f71009841c7f779aee852be56
Pedro Devoto <pedro.devoto@gmail.com>
2014-10-16 14:10:53 -0300

    fix

--- get-json-estadisticas_automotor.php
+++ get-json-estadisticas_automotor.php
@@ -6,7 +6,7 @@
 require_once('Connections/connection.php');
 require_once('inc/db_functions.php');
 
-$colors = ["#f45b5b", "#8085e9", "#8d4654", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee", "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"];
+$colors = array("#f45b5b", "#8085e9", "#8d4654", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee", "#55BF3B", "#DF5353", "#7798BF", "#aaeeee");
 
 $output = array();
 
